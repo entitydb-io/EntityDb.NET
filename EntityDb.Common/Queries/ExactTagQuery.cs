106ce681ac6f43c2d02efe2c110ef14e61d92d89
Chris Philips <the.avid.engineer@outlook.com>
2021-06-18 14:04:22 -0700

    feature: query for an exact tag

---
+++ EntityDb.Common/Queries/ExactTagQuery.cs
@@ -0,0 +1,31 @@
+﻿using EntityDb.Abstractions.Queries;
+using EntityDb.Abstractions.Queries.FilterBuilders;
+using EntityDb.Abstractions.Queries.SortBuilders;
+using EntityDb.Abstractions.Tags;
+using EntityDb.Common.Tags;
+
+namespace EntityDb.Common.Queries
+{
+    /// <summary>
+    /// A query for the exact tag.
+    /// </summary>
+    public sealed record ExactTagQuery(ITag Tag) : ITagQuery
+    {
+        public TFilter GetFilter<TFilter>(ITagFilterBuilder<TFilter> builder)
+        {
+            return builder.TagMatches<Tag>((x) =>
+                x.Scope == Tag.Scope &&
+                x.Label == Tag.Label &&
+                x.Value == Tag.Value);
+        }
+
+        public TSort? GetSort<TSort>(ITagSortBuilder<TSort> builder)
+        {
+            return default;
+        }
+
+        public int? Skip => null;
+
+        public int? Take => 1;
+    }
+}
