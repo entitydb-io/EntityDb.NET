5d5ea051cc67438986bdba3a6cb1698b5b9c8534
Chris Philips <the.avid.engineer@outlook.com>
2021-08-18 21:42:11 -0700

    chore: add xml comments to logging interfaces

--- EntityDb.Abstractions/Loggers/ILogger.cs
+++ EntityDb.Abstractions/Loggers/ILogger.cs
@@ -2,8 +2,16 @@
 
 namespace EntityDb.Abstractions.Loggers
 {
+    /// <summary>
+    /// Represents a type that logs errors (and possibly other things in the future?).
+    /// </summary>
     public interface ILogger
     {
+        /// <summary>
+        /// Logs a caught <see cref="Exception"/> along with human-readable message about what was attempted before the exception was thrown.
+        /// </summary>
+        /// <param name="exception">The exception that was caught.</param>
+        /// <param name="message">The human-readable message about what was attempted before <paramref name="exception"/> was thrown.</param>
         void LogError(Exception exception, string message);
     }
 }
--- EntityDb.Abstractions/Loggers/ILoggerFactory.cs
+++ EntityDb.Abstractions/Loggers/ILoggerFactory.cs
@@ -2,8 +2,16 @@
 
 namespace EntityDb.Abstractions.Loggers
 {
+    /// <summary>
+    /// Represents a type used to create a <see cref="ILogger"/>.
+    /// </summary>
     public interface ILoggerFactory
     {
+        /// <summary>
+        /// Returns a new <see cref="ILogger"/>.
+        /// </summary>
+        /// <param name="type">The type that will be using the logger.</param>
+        /// <returns>A new <see cref="ILogger"/>.</returns>
         ILogger CreateLogger(Type type);
     }
 }
