7d20b57caed8ad256c341ef465d2a3cfa1684a0e
Chris Philips <the.avid.engineer@outlook.com>
2021-09-30 15:11:21 -0700

    feature: create collections direct
    mongodb://localhost:27017

---
+++ EntityDb.MongoDb.Provisioner/Commands/CreateCollectionsDirect.cs
@@ -0,0 +1,36 @@
+using MongoDB.Driver;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Threading.Tasks;
+using EntityDb.MongoDb.Extensions;
+
+namespace EntityDb.MongoDb.Provisioner.Commands
+{
+    public class CreateCollectionsDirect : CommandBase
+    {
+        public static void AddTo(RootCommand rootCommand)
+        {
+            var createCollectionsDirect = new Command("create-collections-direct");
+
+            AddEntityNameArgumentTo(createCollectionsDirect);
+            
+            var connectionStringArgument = new Argument<string>("connection-string", "The connection string to the mongodb instance.");
+
+            createCollectionsDirect.AddArgument(connectionStringArgument);
+
+            createCollectionsDirect.Handler = CommandHandler.Create(async (string entityName, string connectionString) =>
+            {
+                await Execute( entityName, connectionString);
+            });
+
+            rootCommand.AddCommand(createCollectionsDirect);
+        }
+
+        public static async Task Execute(string entityName, string connectionString)
+        {
+            var mongoClient = new MongoClient(connectionString);
+
+            await mongoClient.ProvisionCollections(entityName);
+        }
+    }
+}
