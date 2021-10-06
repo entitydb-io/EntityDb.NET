0bef28e3d397aa56c1a366bbb0681e62c589da0a
Chris Philips <the.avid.engineer@outlook.com>
2021-10-06 14:22:34 -0700

    chore: add XML Doc Comments for ITransactionSubscriber

--- src/EntityDb.Abstractions/Transactions/ITransactionSubscriber.cs
+++ src/EntityDb.Abstractions/Transactions/ITransactionSubscriber.cs
@@ -1,15 +1,15 @@
 namespace EntityDb.Abstractions.Transactions
 {
     /// <summary>
-    /// 
+    ///     Represents a type that reacts to transactions that have been committed.
     /// </summary>
     /// <typeparam name="TEntity"></typeparam>
     public interface ITransactionSubscriber<TEntity>
     {
         /// <summary>
-        /// Foo
+        ///     Called when a transaction has been committed (or on replay, if used in that way).
         /// </summary>
-        /// <param name="transaction"></param>
+        /// <param name="transaction">The committed transaction.</param>
         void Notify(ITransaction<TEntity> transaction);
     }
 }
