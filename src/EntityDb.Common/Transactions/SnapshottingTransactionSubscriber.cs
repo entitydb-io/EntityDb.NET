9a03555d5412315c0135fdca4b2144a148a7f890
Chris Philips <the.avid.engineer@outlook.com>
2021-10-06 09:43:18 -0700

    feature: snapshotting transaction subscriber
    store transaction snapshots asynchronously, don't block or allow a failure to cause problems :)

---
+++ src/EntityDb.Common/Transactions/SnapshottingTransactionSubscriber.cs
@@ -0,0 +1,48 @@
+using EntityDb.Abstractions.Snapshots;
+using EntityDb.Abstractions.Transactions;
+using EntityDb.Common.Snapshots;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityDb.Common.Transactions
+{
+    internal class SnapshottingTransactionSubscriber<TEntity> : AsyncTransactionSubscriber<TEntity>
+    {
+        private readonly ISnapshotRepositoryFactory<TEntity> _snapshotRepositoryFactory;
+        private readonly ISnapshotSessionOptions _snapshotSessionOptions = new SnapshotSessionOptions();
+
+        public SnapshottingTransactionSubscriber
+        (
+            ISnapshotRepositoryFactory<TEntity> snapshotRepositoryFactory,
+            bool testMode
+        ) : base(testMode)
+        {
+            _snapshotRepositoryFactory = snapshotRepositoryFactory;
+        }
+
+        protected override async Task NotifyAsync(ITransaction<TEntity> transaction)
+        {
+            var commandGroups = transaction.Commands
+                .GroupBy(command => command.EntityId);
+                
+            foreach (var commandGroup in commandGroups)
+            {
+                var entityId = commandGroup.Key;
+                var nextSnapshot = commandGroup.Last().NextSnapshot;
+            
+                await using var snapshotRepository =
+                    await _snapshotRepositoryFactory.CreateRepository(_snapshotSessionOptions);
+            
+                await snapshotRepository.PutSnapshot(entityId, nextSnapshot);
+            }
+        }
+
+        public static SnapshottingTransactionSubscriber<TEntity> Create(IServiceProvider serviceProvider, bool testMode)
+        {
+            return ActivatorUtilities.CreateInstance<SnapshottingTransactionSubscriber<TEntity>>(serviceProvider,
+                testMode);
+        }
+    }
+}
