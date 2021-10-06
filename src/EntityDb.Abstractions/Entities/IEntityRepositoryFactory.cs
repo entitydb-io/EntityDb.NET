3e1a7ada6ae0285ac3d426b219809c916a23c4fd
Chris Philips <the.avid.engineer@outlook.com>
2021-10-06 14:19:49 -0700

    chore: add XML Documentation Comments for IEntityRepositoryFactory

--- src/EntityDb.Abstractions/Entities/IEntityRepositoryFactory.cs
+++ src/EntityDb.Abstractions/Entities/IEntityRepositoryFactory.cs
@@ -4,8 +4,18 @@ using System.Threading.Tasks;
 
 namespace EntityDb.Abstractions.Entities
 {
+    /// <summary>
+    ///     Represents a type used to create instances of <see cref="IEntityRepository{TEntity}" />
+    /// </summary>
+    /// <typeparam name="TEntity">The type of entity managed by the <see cref="IEntityRepository{TEntity}" />.</typeparam>
     public interface IEntityRepositoryFactory<TEntity>
     {
+        /// <summary>
+        ///     Create a new instance of <see cref="IEntityRepository{TEntity}" />
+        /// </summary>
+        /// <param name="transactionSessionOptions">The agent's use case for the inner transaction repository.</param>
+        /// <param name="snapshotSessionOptions">The agent's use case for the inner snapshot repository.</param>
+        /// <returns>A new instance of <see cref="IEntityRepository{TEntity}" />.</returns>
         Task<IEntityRepository<TEntity>> CreateRepository(ITransactionSessionOptions transactionSessionOptions,
             ISnapshotSessionOptions? snapshotSessionOptions = null);
     }
