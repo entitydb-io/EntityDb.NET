5525ec3fb2f787009d0e4241b9825a67e03e3d83
Chris Philips <the.avid.engineer@outlook.com>
2021-12-19 22:45:09 -0800

    refactor: ugh shut up codacy

--- src/EntityDb.Void/Transactions/VoidTransactionRepository.cs
+++ src/EntityDb.Void/Transactions/VoidTransactionRepository.cs
@@ -12,12 +12,6 @@ namespace EntityDb.Void.Transactions
 {
     internal sealed class VoidTransactionRepository<TEntity> : ITransactionRepository<TEntity>
     {
-        private static readonly Task<Guid[]> EmptyGuidArrayTask = Task.FromResult(Array.Empty<Guid>());
-        private static readonly Task<object[]> EmptyObjectArrayTask = Task.FromResult(Array.Empty<object>());
-        private static readonly Task<ILease[]> EmptyLeaseArrayTask = Task.FromResult(Array.Empty<ILease>());
-        private static readonly Task<ITag[]> EmptyTagArrayTask = Task.FromResult(Array.Empty<ITag>());
-        private static readonly Task<bool> TrueTask = Task.FromResult(true);
-
         private static readonly Task<ICommand<TEntity>[]> _emptyCommandArrayTask =
             Task.FromResult(Array.Empty<ICommand<TEntity>>());
 
@@ -26,78 +20,78 @@ namespace EntityDb.Void.Transactions
 
         public Task<Guid[]> GetTransactionIds(ISourceQuery sourceQuery)
         {
-            return EmptyGuidArrayTask;
+            return Task.FromResult(Array.Empty<Guid>());
         }
 
         public Task<Guid[]> GetTransactionIds(ICommandQuery commandQuery)
         {
-            return EmptyGuidArrayTask;
+            return Task.FromResult(Array.Empty<Guid>());
         }
 
         public Task<Guid[]> GetTransactionIds(ILeaseQuery leaseQuery)
         {
-            return EmptyGuidArrayTask;
+            return Task.FromResult(Array.Empty<Guid>());
         }
 
         public Task<Guid[]> GetTransactionIds(ITagQuery tagQuery)
         {
-            return EmptyGuidArrayTask;
+            return Task.FromResult(Array.Empty<Guid>());
         }
 
         public Task<Guid[]> GetEntityIds(ISourceQuery sourceQuery)
         {
-            return EmptyGuidArrayTask;
+            return Task.FromResult(Array.Empty<Guid>());
         }
 
         public Task<Guid[]> GetEntityIds(ICommandQuery commandQuery)
         {
-            return EmptyGuidArrayTask;
+            return Task.FromResult(Array.Empty<Guid>());
         }
 
         public Task<Guid[]> GetEntityIds(ILeaseQuery leaseQuery)
         {
-            return EmptyGuidArrayTask;
+            return Task.FromResult(Array.Empty<Guid>());
         }
 
         public Task<Guid[]> GetEntityIds(ITagQuery tagQuery)
         {
-            return EmptyGuidArrayTask;
+            return Task.FromResult(Array.Empty<Guid>());
         }
 
         public Task<object[]> GetSources(ISourceQuery sourceQuery)
         {
-            return EmptyObjectArrayTask;
+            return Task.FromResult(Array.Empty<object>());
         }
 
         public Task<ICommand<TEntity>[]> GetCommands(ICommandQuery commandQuery)
         {
-            return _emptyCommandArrayTask;
+            return Task.FromResult(Array.Empty<ICommand<TEntity>>());
         }
 
         public Task<ILease[]> GetLeases(ILeaseQuery leaseQuery)
         {
-            return EmptyLeaseArrayTask;
+            return Task.FromResult(Array.Empty<ILease>());
         }
 
         public Task<ITag[]> GetTags(ITagQuery tagQuery)
         {
-            return EmptyTagArrayTask;
+            return Task.FromResult(Array.Empty<ITag>());
         }
 
         public Task<IEntityAnnotation<ICommand<TEntity>>[]> GetAnnotatedCommands(ICommandQuery commandQuery)
         {
-            return _emptyAnnotatedCommandArrayTask;
+            return Task.FromResult(Array.Empty<IEntityAnnotation<ICommand<TEntity>>>());
         }
 
         public Task<bool> PutTransaction(ITransaction<TEntity> transaction)
         {
-            return TrueTask;
+            return Task.FromResult(true);
         }
 
         [ExcludeFromCodeCoverage]
         public void Dispose()
         {
-            // Nothing To Dispose
+            DisposeAsync().AsTask().Wait();
         }
 
         public ValueTask DisposeAsync()
