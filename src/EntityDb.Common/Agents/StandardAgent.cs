960f2b505137da8d07f76d976d89f42c100ce036
Chris Philips <the.avid.engineer@outlook.com>
2022-06-25 09:41:21 -0700

    refactor: standard agent object for internal use

---
+++ src/EntityDb.Common/Agents/StandardAgent.cs
@@ -0,0 +1,9 @@
+﻿using EntityDb.Abstractions.Agents;
+using EntityDb.Abstractions.ValueObjects;
+
+namespace EntityDb.Common.Agents;
+
+internal sealed record StandardAgent(object Signature) : IAgent
+{
+    public TimeStamp TimeStamp => TimeStamp.UtcNow;
+}
