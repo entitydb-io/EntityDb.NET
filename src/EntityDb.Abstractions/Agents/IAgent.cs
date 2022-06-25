dfc9a0b5f3a2ad2e1b1221d07fcc4737898f0146
Chris Philips <the.avid.engineer@outlook.com>
2022-06-25 09:35:31 -0700

    refactor: agent has properties instead of methods
    we're not serializing the agent as a whole, so the methods are not necessary

--- src/EntityDb.Abstractions/Agents/IAgent.cs
+++ src/EntityDb.Abstractions/Agents/IAgent.cs
@@ -7,16 +7,7 @@ namespace EntityDb.Abstractions.Agents;
 /// </summary>
 public interface IAgent
 {
-    /// <summary>
-    ///     Returns the timestamp, as decided by the agent.
-    /// </summary>
-    /// <returns></returns>
-    TimeStamp GetTimeStamp();
+    TimeStamp TimeStamp { get; }
 
-    /// <summary>
-    ///     Returns an object that represents the signature of the agent.
-    /// </summary>
-    /// <param name="signatureOptionsName">The name of the signature options configuration.</param>
-    /// <returns>An object that represents the signature of the agent.</returns>
-    object GetSignature(string signatureOptionsName);
+    object Signature { get; }
 }
