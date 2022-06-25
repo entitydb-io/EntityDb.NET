d887a6454d5eab4ea888c5e73f1ee4141a931024
Chris Philips <the.avid.engineer@outlook.com>
2022-06-25 09:34:20 -0700

    feat: abstractions for transaction builders & factory

---
+++ src/EntityDb.Abstractions/Transactions/Builders/ITransactionBuilderFactory.cs
@@ -0,0 +1,11 @@
+﻿using EntityDb.Abstractions.ValueObjects;
+using EntityDb.Common.Transactions.Builders;
+using System.Threading.Tasks;
+
+namespace EntityDb.Abstractions.Transactions.Builders;
+
+public interface ITransactionBuilderFactory<TEntity>
+{
+    Task<ITransactionBuilder<TEntity>> Create(string agentSignatureOptionsName);
+    Task<ISingleEntityTransactionBuilder<TEntity>> CreateForSingleEntity(string agentSignatureOptionsName, Id entityId);
+}
