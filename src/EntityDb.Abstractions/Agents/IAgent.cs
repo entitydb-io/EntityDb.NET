1acd57e5e417ea747d911ec86eceb544e5e53855
Chris Philips <the.avid.engineer@outlook.com>
2022-06-25 09:39:14 -0700

    chore: missing some doc comments

--- src/EntityDb.Abstractions/Agents/IAgent.cs
+++ src/EntityDb.Abstractions/Agents/IAgent.cs
@@ -7,7 +7,13 @@ namespace EntityDb.Abstractions.Agents;
 /// </summary>
 public interface IAgent
 {
+    /// <summary>
+    ///     Gets the current timestamp, according to the agent.
+    /// </summary>
     TimeStamp TimeStamp { get; }
 
+    /// <summary>
+    ///     Gets the signature of the agent, can can be serialized to a database.
+    /// </summary>
     object Signature { get; }
 }
