02f1476290ea6aa517044545f3cb7a78e224ec83
Chris Philips <the.avid.engineer@outlook.com>
2022-06-25 23:44:58 -0700

    feat: pointer record

---
+++ src/EntityDb.Abstractions/ValueObjects/Pointer.cs
@@ -0,0 +1,8 @@
+﻿namespace EntityDb.Abstractions.ValueObjects;
+
+/// <summary>
+///     Points to an object (e.g., entity, projection)
+/// </summary>
+/// <param name="Id">The id of the object.</param>
+/// <param name="VersionNumber">The version number of the object.</param>
+public record struct Pointer(Id Id, VersionNumber VersionNumber);
