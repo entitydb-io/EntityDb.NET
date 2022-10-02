8d5c7e9b99c3ed25c184a33a1eb9007e3b4c3762
Chris Philips <the.avid.engineer@outlook.com>
2022-10-02 15:02:44 -0700

    feat: more logs (#74)
    log when transaction starts, commits, aborts

--- src/EntityDb.MongoDb/Sessions/MongoSession.cs
+++ src/EntityDb.MongoDb/Sessions/MongoSession.cs
@@ -180,12 +180,20 @@ internal record MongoSession
     public void StartTransaction()
     {
         AssertNotReadOnly();
-
+        
         ClientSessionHandle.StartTransaction(new TransactionOptions
         (
             writeConcern: WriteConcern,
             maxCommitTime: Options.WriteTimeout
         ));
+
+        Logger
+            .LogInformation
+            (
+                "Started MongoDb Transaction on `{DatabaseNamespace}`\n\nServer SessionId: {ServerSessionId}",
+                MongoDatabase.DatabaseNamespace,
+                ClientSessionHandle.ServerSession.Id.ToString()
+            );
     }
 
     [ExcludeFromCodeCoverage(Justification =
@@ -195,6 +203,14 @@ internal record MongoSession
         AssertNotReadOnly();
 
         await ClientSessionHandle.CommitTransactionAsync(cancellationToken);
+
+        Logger
+            .LogInformation
+            (
+                "Committed MongoDb Transaction on `{DatabaseNamespace}`\n\nServer SessionId: {ServerSessionId}",
+                MongoDatabase.DatabaseNamespace,
+                ClientSessionHandle.ServerSession.Id.ToString()
+            );
     }
 
     public async Task AbortTransaction()
@@ -202,6 +218,14 @@ internal record MongoSession
         AssertNotReadOnly();
 
         await ClientSessionHandle.AbortTransactionAsync();
+
+        Logger
+            .LogInformation
+            (
+                "Aborted MongoDb Transaction on `{DatabaseNamespace}`\n\nServer SessionId: {ServerSessionId}",
+                MongoDatabase.DatabaseNamespace,
+                ClientSessionHandle.ServerSession.Id.ToString()
+            );
     }
 
     public override ValueTask DisposeAsync()
