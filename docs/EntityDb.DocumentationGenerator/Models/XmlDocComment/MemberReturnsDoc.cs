1cfe064d2a6e1cb857f6a9622bffd2ac8f65e410
Chris Philips <the.avid.engineer@outlook.com>
2022-12-03 20:22:46 -0800

    wip

--- docs/EntityDb.DocumentationGenerator/Models/XmlDocComment/MemberReturnsDoc.cs
+++ docs/EntityDb.DocumentationGenerator/Models/XmlDocComment/MemberReturnsDoc.cs
@@ -1,7 +1,4 @@
-﻿using System.Xml;
-using System.Xml.Serialization;
-
-namespace EntityDb.DocumentationGenerator.Models.XmlDocComment;
+﻿namespace EntityDb.DocumentationGenerator.Models.XmlDocComment;
 
 public class MemberReturnsDoc : DocWithMixedInnerXml
 {
